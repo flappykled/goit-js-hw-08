3363534f2914f6cb6ba228c40ce0143740b5ffec
flappykled <dima04089804@gmail.com>
2022-12-14 19:59:26 +0200

    hmw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
-const gallery = document.querySelector(`.gallery`);
-const item = [];
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const items = [];
 
 galleryItems.forEach(element => {
-    const galleryLink = document.createElement(`a`);
-    galleryLink.className = `gallert__link`;
-    galleryLink.href = element.original;
-    const galleryImage = document.createElement(`img`);
-    galleryImage.className = 'gallery__image';
-    galleryImage.src = element.preview;
-    galleryImage.setAttribute('title', element.description);
-    galleryImage.alt = element.description;
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute('title', element.description);
+  galleryImage.alt = element.description;
 
-    galleryLink.append(galleryImage);
-    item.push(galleryLink);
+  galleryLink.append(galleryImage);
+  items.push(galleryLink);
 });
-gallery.append(...item);
+gallery.append(...items);
 
 new SimpleLightbox('.gallery a', {
   captionDelay: 250,
