d157edeca3f2d8f88d73b00486cbabb13ec589e0
flappykled <dima04089804@gmail.com>
2022-12-14 19:57:24 +0200

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+// Change code below this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Change code below this line
 
 console.log(galleryItems);
 
